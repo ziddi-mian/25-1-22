185023bb34f65a9ac1a1f2456975955008f4f54f
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-25 12:27:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -32,15 +32,12 @@ dt = new Date(1989, 10, 1);
 console.log(daylights_savings(dt));\
 
 
-
-
-
-
 // Write a JavaScript function to get difference to Greenwich time (GMT) in hours.   
 // Test Data :
 // dt = new Date();
 // console.log(diff_to_GMT(dt));
 // "+05.500"
+
 
 
 function diff_to_GMT(dt) 
