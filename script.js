a34d7546a4e753afba1b6f2d6ee402e1ee5d25c3
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-25 12:31:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -75,8 +75,6 @@ console.log(timezone_offset_in_seconds(dt));
 // "Sat Nov 02 2024 00:00:00 GMT+0530 (India Standard Time)"
 
 
-
-
 function add_years(dt,n) 
  {
  return new Date(dt.setFullYear(dt.getFullYear() + n));      
@@ -86,7 +84,9 @@ dt = new Date();
 console.log(add_years(dt, 10).toString());  
 
 dt = new Date(2014,10,2);
+
 console.log(add_years(dt, 10).toString());
+
 
 
 
